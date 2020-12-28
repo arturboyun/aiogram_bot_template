a19d5981f5acf0fbe003efd563361287cee0111c
arturboyun <arturboyun@gmail.com>
2020-12-29 00:48:54 +0200

    fix: remove venv/ from repo

--- venv/Lib/site-packages/tortoise/backends/sqlite/__init__.py
+++
@@ -1,3 +0,0 @@
-from .client import SqliteClient
-
-client_class = SqliteClient
--- venv/Lib/site-packages/tortoise/contrib/pydantic/__init__.py
+++
@@ -1,5 +0,0 @@
-from tortoise.contrib.pydantic.base import PydanticListModel, PydanticModel  # noqa
-from tortoise.contrib.pydantic.creator import (  # noqa
-    pydantic_model_creator,
-    pydantic_queryset_creator,
-)
